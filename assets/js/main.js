1fcda5ed4bd1c3ebbf0320bded96fc61034e5c7a
David Milivojev <davidmilivojev@gmail.com>
2023-08-20 12:24:12 +0200

    Header states

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,35 @@
+var headerSticky = 'header--sticky';
+var $header = $('.js-header');
+var headerLight = 'header--light';
+var $toggle = $('.js-toggle');
+var toggleActive = 'toggle--active';
+
 function navigation() {
-    var $toggle = $('.js-toggle');
     var $nav = $('.js-nav');
-    var $header = $('.js-header');
-    var toggleActive = 'toggle--active';
-    var headerLight = 'header--light';
 
     $toggle.on('click', function() {
         $(this).toggleClass(toggleActive);
-        $header.toggleClass(headerLight);
         $nav.stop().slideToggle();
+        if(!$header.hasClass(headerSticky)) {
+            $header.toggleClass(headerLight);
+        }
     });
 }
 
+function stickyHeader() {
+    $(window).on('scroll', function() {
+        var scrollTop = $(this).scrollTop();
+        if(scrollTop > 0) {
+            if(!$toggle.hasClass(toggleActive)) {
+                $header.addClass([headerLight, headerSticky]);
+            }
+        } else {
+            if(!$toggle.hasClass(toggleActive)) {
+                $header.removeClass([headerLight, headerSticky]);
+            }
+        }
+    });
+}
+
+stickyHeader()
 navigation();
